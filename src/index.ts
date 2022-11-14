27af7d83d3c0a343c28b8872c5dde5be86141d49
ilhamadi77 <ilham.adi78@gmail.com>
2022-11-14 22:21:35 +0700

    next tsc v0.1

--- src/index.ts
+++ src/index.ts
@@ -1,61 +1,24 @@
-import { v4 as uuidV4 } from "uuid"
+import express from "express";
+const app = express()
 
-type Task = {
-  id: string
-  title: string
-  completed: boolean
-  createdAt: Date
-}
-
-const list = document.querySelector<HTMLUListElement>("#list")
-const form = document.getElementById("new-task-form") as HTMLFormElement | null
-const input = document.querySelector<HTMLInputElement>("#new-task-title")
-const tasks: Task[] = loadTasks()
-tasks.forEach(addListItem)
-
-form?.addEventListener("submit", e => {
-  e.preventDefault()
-
-  if (input?.value == "" || input?.value == null) return
-
-  const newTask: Task = {
-    id: uuidV4(),
-    title: input.value,
-    completed: false,
-    createdAt: new Date(),
-  }
-  tasks.push(newTask)
-  saveTasks()
-
-  addListItem(newTask)
-  input.value = ""
+app.use(express.json())
+app.get("/", (req, res) => {
+  return res.send("hello word ts with express")
+})
+app.get("/test", (req, res) => {
+  return res.send("hello word ts with express test")
 })
 
-function addListItem(task: Task) {
-  const item = document.createElement("li")
-  const label = document.createElement("label")
-  const checkbox = document.createElement("input")
-  checkbox.addEventListener("change", () => {
-    task.completed = checkbox.checked
-    saveTasks()
-  })
-  checkbox.type = "checkbox"
-  checkbox.checked = task.completed
-  label.append(checkbox, task.title)
-  item.append(label)
-  list?.append(item)
-}
+app.post("/api/data", (req, res) => {
+  console.log(req.body)
 
-function saveTasks() {
-  localStorage.setItem("TASKS", JSON.stringify(tasks))
-}
+  return res.sendStatus(200)
+})
+app.all("/api/all", (req, res) => {
+  return res.sendStatus(200)
+})
 
-function loadTasks(): Task[] {
-  const taskJSON = localStorage.getItem("TASKS")
-  if (taskJSON == null) return []
-  return JSON.parse(taskJSON)
-}
 
-function hapus():any{
-  localStorage.clear()
-}
+app.listen(4001, () => {
+  console.log("Server running in localhost:4001")
+})
